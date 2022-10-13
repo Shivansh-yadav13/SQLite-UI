1ac24064b82f8ce1b723fa4e6364f0d84f24ea70
Г. Магнайбаяр <47246554+MagnaibayarMN@users.noreply.github.com>
2022-10-13 16:31:49 +0800

    test: added basic datatype tests for AddColumn function (#25) [ci skip]

--- db/database_test.go
+++ db/database_test.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -66,5 +68,109 @@ func TestGetSQLQuery(t *testing.T) {
 	}
 	if sql == "" {
 		t.Errorf("Table schema should not be empty")
+	}
+}
+
+/*
+	Test suite for AddColumn function
+*/
+func TestAddColumnForText(t *testing.T) {
+	if err := ConnectDB("../sqlite_database/data.db"); err != nil {
+		t.Error(err)
+	}
+	newTableName := "tbl_schema_test"
+	newColumnName := "demo_text"
+	newColumnType := "TEXT"
+	if err := AddColumn(newTableName, newColumnName, newColumnType); err != nil {
+		t.Fatal(err)
+	}
+
+	sql := "SELECT sql FROM sqlite_schema where tbl_name=?"
+	sql, err := GetSQLQuery(newTableName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sql == "" {
+		t.Errorf("Table schema should not be empty")
+	}
+
+	t.Log(sql)
+	if !strings.Contains(sql, fmt.Sprintf("'%s' '%s'", newColumnName, newColumnType)) {
+		t.Errorf("Column is not added correctly")
+	}
+}
+
+func TestAddColumnForNull(t *testing.T) {
+	if err := ConnectDB("../sqlite_database/data.db"); err != nil {
+		t.Error(err)
+	}
+	newTableName := "tbl_schema_test"
+	newColumnName := "demo_null"
+	newColumnType := "NULL"
+	if err := AddColumn(newTableName, newColumnName, newColumnType); err != nil {
+		t.Fatal(err)
+	}
+
+	sql := "SELECT sql FROM sqlite_schema where tbl_name=?"
+	sql, err := GetSQLQuery(newTableName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sql == "" {
+		t.Errorf("Table schema should not be empty")
+	}
+
+	if !strings.Contains(sql, fmt.Sprintf("'%s' '%s'", newColumnName, newColumnType)) {
+		t.Errorf("Column is not added correctly")
+	}
+}
+
+func TestAddColumnForReal(t *testing.T) {
+	if err := ConnectDB("../sqlite_database/data.db"); err != nil {
+		t.Error(err)
+	}
+	newTableName := "tbl_schema_test"
+	newColumnName := "demo_real"
+	newColumnType := "REAL"
+	if err := AddColumn(newTableName, newColumnName, newColumnType); err != nil {
+		t.Fatal(err)
+	}
+
+	sql := "SELECT sql FROM sqlite_schema where tbl_name=?"
+	sql, err := GetSQLQuery(newTableName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sql == "" {
+		t.Errorf("Table schema should not be empty")
+	}
+
+	if !strings.Contains(sql, fmt.Sprintf("'%s' '%s'", newColumnName, newColumnType)) {
+		t.Errorf("Column is not added correctly")
+	}
+}
+
+func TestAddColumnForBlob(t *testing.T) {
+	if err := ConnectDB("../sqlite_database/data.db"); err != nil {
+		t.Error(err)
+	}
+	newTableName := "tbl_schema_test"
+	newColumnName := "demo_blob"
+	newColumnType := "BLOB"
+	if err := AddColumn(newTableName, newColumnName, newColumnType); err != nil {
+		t.Fatal(err)
+	}
+
+	sql := "SELECT sql FROM sqlite_schema where tbl_name=?"
+	sql, err := GetSQLQuery(newTableName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sql == "" {
+		t.Errorf("Table schema should not be empty")
+	}
+
+	if !strings.Contains(sql, fmt.Sprintf("'%s' '%s'", newColumnName, newColumnType)) {
+		t.Errorf("Column is not added correctly")
 	}
 }
